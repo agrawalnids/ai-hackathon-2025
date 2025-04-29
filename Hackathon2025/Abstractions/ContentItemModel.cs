4b691604de66b9f35099948bd574198f16ff6358
Zarna Grundy <zarnasharma92@gmail.com>
2025-04-28 22:03:03 -0400

    Add Discovery feed

---
+++ Hackathon2025/Abstractions/ContentItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon2025.Abstractions
+{
+    public class ContentItemModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string IsReal { get; set; }
+    }
+}
